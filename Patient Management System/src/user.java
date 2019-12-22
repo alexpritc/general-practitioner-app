767e2a974321c30464acd883cefc6e76fc5530c9
Alex Pritchard <alexpritc@users.noreply.github.com>
2019-12-22 00:06:09 +0000

    Added user class that each account type extends.

---
+++ Patient Management System/src/user.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alex Pritchard
+ */
+public class user {
+    
+    private String name;
+    private String surname;
+    private String address;
+    private String id;
+    
+    public user(String name, String surname, String address, String id) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}

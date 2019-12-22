3c256d264e226dbdf0f1b470da379ef4c9d3584e
Alex Pritchard <alexpritc@users.noreply.github.com>
2019-12-22 00:05:51 +0000

    Added doctor class.

---
+++ Patient Management System/src/doctor.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alex Pritchard
+ */
+public class doctor extends user{
+    
+    public doctor(String name, String surname, String address, String id){
+        super(name, surname, address, id);
+    }
+}

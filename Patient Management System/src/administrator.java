98f5fb4a161f3f3373b4f98ab7a441cb0b099f60
Alex Pritchard <alexpritc@users.noreply.github.com>
2019-12-22 00:05:07 +0000

    Added administrator class.

---
+++ Patient Management System/src/administrator.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alex Pritchard
+ */
+public class administrator extends user{
+    
+    public administrator(String name, String surname, String address, String id){
+        super(name, surname, address, id);
+    }
+}
+
